35061f23ab585205cf24541ecca84683b651b86c
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-02-19 22:16:30 +0530

    frontend

--- 02-sg/sg.tf
+++ 02-sg/sg.tf
@@ -257,12 +257,22 @@ resource "aws_security_group_rule" "bastion_public" {
 
 ### tools ###
 ## backend accepting connection from default_vpc ##
-resource "aws_security_group_rule" "backend_default_subnet" {
+resource "aws_security_group_rule" "backend_default_vpc" {
     type = "ingress"
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["172.31.0.0/16"]
     security_group_id = module.backend.sg_id 
+}
+
+## frontend accepting connection from default_vpc ##
+resource "aws_security_group_rule" "frontend_default_vpc" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+    security_group_id = module.frontend.sg_id 
 }
 
