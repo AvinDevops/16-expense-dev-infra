c7a93d9daa41f2a03a46e88f0fbc0b12414ac3ef
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-02-19 21:45:58 +0530

    sg

--- 02-sg/sg.tf
+++ 02-sg/sg.tf
@@ -262,7 +262,7 @@ resource "aws_security_group_rule" "backend_default_subnet" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["172.31.16.0/20"]
+    cidr_blocks = ["172.31.0.0/16"]
     security_group_id = module.backend.sg_id 
 }
 
